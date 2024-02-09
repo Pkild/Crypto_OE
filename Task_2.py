e534881988a5e1190f3d45051054a746811d96aa
Pkild <cccvvvggg@mail.ru>
2024-02-09 22:13:19 +0300

    Task 2 not completed

--- Task_2.py
+++ Task_2.py
@@ -1,2 +1,7 @@
-print("hello")
-print("hey")
+message = '1111000001'
+key = "00110"
+encryption = lambda a, b: a ^ b
+message = list(message)
+key = list(key)
+print(message, key)
+encrypted_message = encryption(message, key)
