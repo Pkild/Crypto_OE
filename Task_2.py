8e6bf26b515f4c06c8ada33f2475dec6c106e164
Pkild <hahaha@jd.k>
2024-02-10 14:04:46 +0300

    Task 2 completed

--- Task_2.py
+++ Task_2.py
@@ -1,7 +1,12 @@
-message = '1111000001'
-key = "00110"
-encryption = lambda a, b: a ^ b
-message = list(message)
-key = list(key)
-print(message, key)
-encrypted_message = encryption(message, key)
+message = '11110001100111011011'
+k_1 = '1001110111'
+k_2 = '1010100100'
+
+
+def feistel(msg, k):
+    msg_l = msg[:len(msg) // 2]
+    msg_r = msg[len(msg) // 2:]
+    return msg_r + f"{int(msg_l, 2) ^ int(k, 2) ^ int(msg_r, 2):0>{len(k)}b}"
+
+
+print(feistel(feistel(message, k_1), k_2))
