b4b3f9d1c913f9d803487766d59537ca908302d7
Pkild <cccvvvggg@mail.ru>
2024-02-11 13:00:13 +0300

    Issue https://github.com/Pkild/Crypto_OE/issues/1#issue-2128254609 resolved

--- Task_1.py
+++ Task_1.py
@@ -1,27 +1,16 @@
-alphabet = "а, б, в, г, д, е, ё, ж, з, и, й, к, л, м, н, о, п, р, с, т, у, ф, х, ц, ч, ш, щ, ъ, ы, ь, э, ю, я".split(
-    ", ")
-alphabet = dict((alphabet[i], i) for i in range(len(alphabet)))  # creating alphabet as a dict of pairs letter: position
-
-
-def get_key(i):  # getting letter in alphabet by it's number
-    for k, v in alphabet.items():
-        if v == i:
-            return k
-    return None
-
+alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
+alphabet_numbers = dict((alphabet[i], i) for i in range(len(alphabet)))  # creating alphabet as a dict of pairs letter: position
 
 key1 = 'задел'  # print key for 1 question
 message1 = 'самосборка'  # print message for 1 question
 
-cypher = ''
 for i in range(len(message1)):
-    cypher += get_key((alphabet[message1[i]] + alphabet[key1[i % len(key1)]]) % len(alphabet))
-print(cypher)
+    print(alphabet[(alphabet_numbers[message1[i]] + alphabet_numbers[key1[i % len(key1)]]) % len(alphabet)], end='')
 
 key2 = 'задел'  # print key for 2 question
 message2 = 'щаруцшуцпл'  # print message for 2 question
-decipher = ''
+
+print()
 
 for i in range(len(message2)):
-    decipher += get_key((alphabet[message2[i]] + len(alphabet) - alphabet[key2[i % len(key2)]]) % len(alphabet))
-print(decipher)
+    print(alphabet[(alphabet_numbers[message2[i]] + len(alphabet) - alphabet_numbers[key2[i % len(key2)]]) % len(alphabet)], end='')
